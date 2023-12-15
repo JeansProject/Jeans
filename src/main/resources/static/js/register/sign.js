4395fd028629d81b95dac234b9094e942c2546bb
siso <gtldhs120@gmail.com>
2023-12-15 12:44:05 +0900

    231215_회원가입 유효성 추가

--- src/main/resources/static/js/register/sign.js
+++ src/main/resources/static/js/register/sign.js
@@ -7,6 +7,94 @@
 5. 서버에서 넘어온 response 확인하기
  */
 
+// 아이디 입력창 정보 가져오기
+let elInputId = document.querySelector('#id');
+// 성공 메세지 정보 가져오기
+let elSuccessMessage = document.querySelector('.success-message');
+// 실패 메세지 정보 가져오기 (글자수 제한)
+let elFailureMessage = document.querySelector('.failure-message');
+// 실패 메세지 정보 가져오기 (영어, 숫자)
+let elFailureMessageTwo = document.querySelector('.failure-message2');
+
+// 비밀번호 입력창 정보 가져오기
+let elInputPassword = document.querySelector('#password');
+// 비밀번호 확인 입력창 정보 가져오기
+let elInputPasswordRetype = document.querySelector('#password-retype');
+// 실패 메세지 정보 가져오기 (비밀번호 불일치)
+let elMismatchMessage = document.querySelector('.mismatch-message');
+// 실패 메세지 정보 가져오기 (8글자 이상, 영문, 숫자, 특수문자 미사용)
+let elStrongPasswordMessage = document.querySelector('.strongPassword-message');
+
+// 아이디 글자수 제한
+function idLength(value) {
+    return value.length >= 4 && value.length <= 12
+}
+// 아이디 영어 또는 숫자만
+function idNumberAndEng(str) {
+    return /^[A-Za-z0-9][A-Za-z0-9]*$/.test(str);
+}
+
+// 비밀번호 8글자 이상, 영문, 숫자, 특수문자 사용
+function strongPassword(str) {
+    return /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/.test(str);
+}
+
+// 비밀번호 확인
+function isMatch(password1, password2) {
+    return password1 === password2;
+}
+
+elInputId.onkeyup = function () {
+    // 값을 입력한 경우
+    if (elInputId.value.length !== 0) {
+        if (idNumberAndEng(elInputId.value) === false) {
+            elSuccessMessage.classList.add('hide');
+            elFailureMessage.classList.add('hide');
+            elFailureMessageTwo.classList.remove('hide');
+        } else if (idLength(elInputId.value) === false) {
+            elSuccessMessage.classList.add('hide');
+            elFailureMessage.classList.remove('hide');
+            elFailureMessageTwo.classList.add('hide');
+        } else if (idLength(elInputId.value) || idNumberAndEng(elInputId.value)) {
+            elSuccessMessage.classList.remove('hide');
+            elFailureMessage.classList.add('hide');
+            elFailureMessageTwo.classList.add('hide');
+        }
+    } else { 
+        // 값을 입력하지 않은 경우 (지웠을 때)
+        elSuccessMessage.classList.add('hide');
+        elFailureMessage.classList.add('hide');
+        elFailureMessageTwo.classList.add('hide');
+    }
+}
+
+elInputPassword.onkeyup = function () {
+    // 값을 입력한 경우
+    if (elInputPassword.value.length !== 0) {
+        if (strongPassword(elInputPassword.value)) {
+            elStrongPasswordMessage.classList.add('hide');
+        } else {
+            elStrongPasswordMessage.classList.remove('hide');
+        }
+    } else {
+        // 값을 입력하지 않은 경우 (지웠을 때)
+        elStrongPasswordMessage.classList.add('hide');
+    }
+}
+
+elInputPasswordRetype.onkeyup = function () {
+    if (elInputPasswordRetype.value.length !== 0) {
+        if (isMatch(elInputPassword.value, elInputPasswordRetype.value)) {
+            elMismatchMessage.classList.add('hide');
+        } else {
+            elMismatchMessage.classList.remove('hide');
+        }
+    } else {
+        // 값을 입력하지 않은 경우 (지웠을 때)
+        elMismatchMessage.classList.add('hide');
+    }
+}
+
 const registerBtn = document.querySelector('#registerBtn');
 registerBtn.addEventListener('click', function () {
     const username = document.querySelector('#username').value;
