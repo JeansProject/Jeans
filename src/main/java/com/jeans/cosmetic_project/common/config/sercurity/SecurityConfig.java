914d046603d59888e63b9f2811b255740608e9ac
pjaemyung91 <p.jaemyung91@gmail.com>
2023-12-29 20:14:54 +0900

    feat: 공통
    1. security maven 추가
    
    ---
    
    fix: 로그인
    
    1. mapper에 바인딩되는 클래스명 수정

--- src/main/java/com/jeans/cosmetic_project/common/config/sercurity/SecurityConfig.java
+++ src/main/java/com/jeans/cosmetic_project/common/config/sercurity/SecurityConfig.java
@@ -1,14 +1,11 @@
 package com.jeans.cosmetic_project.common.config.sercurity;
 
-import java.util.stream.Stream;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.security.web.util.matcher.RequestMatcher;
 
 @Configuration
 @EnableWebSecurity
